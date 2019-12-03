c24a1b44e7c0cc0eafa35182ec115a23f40d81fb
Jeff Anderson <jander99@gmail.com>
2019-12-02 22:47:39 -0500

    First attempt at liquibase db

---
+++ src/main/java/com/github/jander99/cartogram/CartogramApplication.java
@@ -0,0 +1,13 @@
+package com.github.jander99.cartogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartogramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jander99/cartogram/CartogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jander99.cartogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
